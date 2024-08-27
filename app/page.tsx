fe9c653bfc52fa155d2105ca2c1c5b04851947d4
yashxa <yawasthi6113@gmail.com>
2024-08-27 13:33:17 +0530

    NavBar placed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { navItems } from "@/data";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -5,17 +6,17 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/', icon: <FaHome />}
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
